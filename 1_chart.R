712f545531764b9095c5c5f04a494f2ee6b98e5b
jvntron <avrelboy1@gmail.com>
2024-09-30 15:55:15 +0800

    Week 4 class

--- 1_chart.R
+++ 1_chart.R
@@ -15,3 +15,35 @@ ggplot(pg_mean, aes(x=group, y=weight)) +
 ggplot(pg_mean, aes(x="", y=weight, fill= group)) +
   geom_col() +
   coord_polar(theta = "y")
+
+# Draw a Bar Chart pg_mean
+ggplot(pg_mean, aes (x = group, y = weight, fill = group)) + 
+  geom_col() +
+  labs(title="Bar Chart, Color",
+       caption = 'By JB, Data Visualization Course, Tunghai University, 2024') +
+  theme(plot.title = element_text(hjust= 0.5, size = 20))
+
+
+# Draw a Line Chart BOD
+ggplot(BOD, aes (x = Time, y = demand)) + 
+  geom_line() +
+  labs(title= "Line Chart", 
+       caption = 'By JB, Data Visualization Course, Tunghai University, 2024') +
+  theme(plot.title = element_text(hjust= 0.5, size = 20))
+
+# Draw a Count Chart diamonds
+ggplot(diamonds, aes (x = carat)) + 
+  geom_bar() +
+  labs(title= "Bar chart", 
+       caption = 'By JB, Data Visualization Course, Tunghai University, 2024') +
+  theme(plot.title = element_text(hjust= 0.5, size = 20))
+
+# Draw a Histogram diamonds
+ggplot(diamonds, aes (x = carat)) + 
+  geom_histogram() +
+  labs(title= "Histogram chart", 
+       caption = 'By JB, Data Visualization Course, Tunghai University, 2024') +
+  theme(plot.title = element_text(hjust= 0.5, size = 20))
+
+# Read Data from CSV
+df <- read.csv('Accidents.csv')
